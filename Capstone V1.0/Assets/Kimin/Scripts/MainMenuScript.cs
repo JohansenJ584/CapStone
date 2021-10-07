ddecd27ad60237f2b8dff779bad3dce9d9679389
Kimin Lee <kiminlee2000225@gmail.com>
2021-10-07 10:04:09 -0400

    MainMenuScript WIP I

--- Capstone V1.0/Assets/Kimin/Scripts/MainMenuScript.cs
+++ Capstone V1.0/Assets/Kimin/Scripts/MainMenuScript.cs
@@ -1,15 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum MainMenuState
+{
+    Main, Controls
+}
 
 public class MainMenuScript : MonoBehaviour
 {
     public GameObject main;
     public GameObject options;
+    UIActions action;
+    MainMenuState state;
+
+    private void Awake()
+    {
+        state = MainMenuState.Main;
+        action = new UIActions();
+    }
+
+    private void OnEnable()
+    {
+        action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        action.Disable();
+    }
+
+    private void Start()
+    {
+        action.Pause.PauseGame.performed += _ => DetermineBack();
+    }
+
+    void DetermineBack()
+    {
+        switch(state)
+        {
+            case MainMenuState.Main:
+                break;
+            case MainMenuState.Controls:
+                Debug.Log("controls escape called");
+                state = MainMenuState.Main;
+                CloseOptions();
+                break;
+            default:
+                break;
+        }
+    }
 
     public void StartGame()
     {
-        
+        SceneManager.LoadScene("Assets/Kimin/Scenes/Combined Build Scene 1");
     }
 
     public void ExitGame()
@@ -21,5 +66,11 @@ public class MainMenuScript : MonoBehaviour
     {
         options.SetActive(true);
         main.SetActive(false);
+    }
+
+    public void CloseOptions()
+    {
+        options.SetActive(false);
+        main.SetActive(true);
     }
 }
