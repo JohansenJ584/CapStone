7f754c4eee0bf44bc2528bacdb1c29699ae0c2da
Jon <jongold00@gmail.com>
2021-11-17 21:12:20 -0500

    More asset imports + DNA Editing UI changes

--- Capstone V1.0/Assets/Jons Stuff/Scripts/ResearchBook.cs
+++ Capstone V1.0/Assets/Jons Stuff/Scripts/ResearchBook.cs
@@ -1,15 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ResearchBook : MonoBehaviour
 {
     [SerializeField]
     GameObject[] tabs;
 
+    [SerializeField]
+    TMP_Dropdown resolutionPicker;
+
+    [SerializeField]
+    Toggle fullscreenToggle;
+
+    #region SoundEffects
+
+    private AudioSource audioSource;
+
+    public AudioClip bookTurn;
+    public AudioClip bookClose; 
+
+    #endregion SoundEffects
 
     public void Start()
     {
+        audioSource = FindObjectOfType<AudioSource>();
         ToggleTab(0);
     }
 
@@ -26,5 +43,46 @@ public class ResearchBook : MonoBehaviour
                 tabs[i].SetActive(false);
             }
         }
+
+        PlayBookTurn();
+
+ 
     }
+
+    public void OnChooseResolution()
+    {
+        switch (resolutionPicker.value)
+        {
+            case 0:
+                Screen.SetResolution(1280, 720, Screen.fullScreen);
+                break;
+            case 1:
+                Screen.SetResolution(1920, 1080, Screen.fullScreen);
+                break;
+            case 2:
+                Screen.SetResolution(2560, 1440, Screen.fullScreen);
+                break;
+        }
+    }
+
+    public void OnToggleFullscreen()
+    {
+        Screen.fullScreen = fullscreenToggle.isOn;
+    }
+
+    #region SoundFunctions
+
+    public void PlayBookTurn()
+    {
+        audioSource.clip = bookTurn;
+        audioSource.Play();
+    }
+
+    public void PlayBookClose()
+    {
+        audioSource.clip = bookClose;
+        audioSource.Play();
+    }
+    #endregion SoundFunctions
 }
+
---
+++ Capstone V1.0/Assets/Jons Stuff/UIKit2/Demo.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Demo : MonoBehaviour 
+{
+    public GameObject[] uis;
+    int id = 0;
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+           
+        }
+	}
+
+    public void Press()
+    {
+        id++;
+        if (id >= uis.Length)
+        {
+            id = 0;
+        }
+
+        for (int m = 0; m < uis.Length; m++)
+        {
+            uis[m].SetActive(false);
+        }
+
+        uis[id].SetActive(true);
+    }
+}
+
