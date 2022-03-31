2d09f0a5581a839fdf0190e42975d4903267c669
JohansenJ584 <54958923+JohansenJ584@users.noreply.github.com>
2022-03-31 11:14:13 -0400

    Added 2 face sounds

---
+++ Capstone V1.0/Assets/RandomCreatureSound.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCreatureSound : MonoBehaviour
+{
+    [SerializeField] AudioSource _audioSource;
+    [SerializeField] List<AudioClip> gruntSounds = new List<AudioClip>();
+    void Start()
+    {
+        InvokeRepeating("MakeSound", 1f, 4f);
+    }
+
+    //The method I'm calling on Update
+    void MakeSound()
+    {
+        if (Random.Range(0, 100f) < 25f)
+        {
+            _audioSource.clip = gruntSounds[Random.Range(0, gruntSounds.Count)];
+            _audioSource.Play();
+        }
+
+    }
+}
